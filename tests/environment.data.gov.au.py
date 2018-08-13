899cae307d0bf4de4e7bbd7b23b279af0db65c15
Nicholas Car <nicholas.car@csiro.au>
2018-08-13 21:04:53 +1000

    empty testing files

---
+++ tests/environment.data.gov.au.py
@@ -0,0 +1,1 @@
+# this file will contain all the tests for environment.data.gov.au
---
+++ tests/main.py
@@ -0,0 +1,5 @@
+# this file will run all the tests in all the domain files
+
+# environment.data.gov.au.py
+# linked.data.gov.au.py
+# reference.data.gov.au.py
