15df8dc7d6f3dbc58b587e43d9ad20fbe8a5cdfd
Nicholas Car <nicholas.car@csiro.au>
2018-08-14 00:11:09 +1000

    removing unused code

--- tests/environment.data.gov.au.py
+++ tests/environment.data.gov.au.py
@@ -18,19 +18,5 @@ def def_ba_glossary():
         'The BA Glossary does not redirect to registry2.it.csiro.au'
 
 
-def def_ba_glossary_html():
-    # get the HTML version of the resource
-    r = requests.get('http://environment.data.gov.au/def/ba/glossary/aquifer')
-
-    # check Content-Type
-    assert 'text/html' in r.headers['Content-Type'], \
-        'Response for HTML does not have Content-Type set to text/html.'
-
-    # check for HTML title ""
-    assert '<title>The Australian Government Records Interoperability Framework (AGRIF) ontology</title>' \
-           in r.content.decode('utf-8'), \
-        'HTML response does not contain the correct <title> element.'
-
-
 if __name__ == '__main__':
     def_ba_glossary()
