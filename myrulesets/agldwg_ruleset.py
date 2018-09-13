2700eae5b5cdcd06a891eb8e15630cb297e417cf
Edmond Chuc <edmond.chuc@csiro.au>
2018-09-13 12:43:14 +1000

    Finished writing tests. Current supported features listed in TODO.md

--- myrulesets/agldwg_ruleset.py
+++ myrulesets/agldwg_ruleset.py
@@ -139,107 +139,266 @@ class RuleInheritanceError(Exception):
 
 class AGLDWG_ValidRuleSet(RuleSet):
     def __init__(self, uri, data):
-        # ruleset_id = uri
         ruleset_name = uri
         rules_results = []
-        
-        metadata = data['metadata']
-        components = data['components']
-        
-        test = ValidateMetadata(uri, metadata)
+        metadata = None
+        components = None
+
+        # check that metadata key exists
+        test = ValidateMetadataIsPresent(uri, data)
         rules_results.append(test)
-        # if not test.passed:
-            # raise RuleInheritanceError(f'Failures: {len(test.fail_reasons)}. Fail reason: {test.fail_reasons}.')
-            
+        if test.passed:
+            metadata = data['metadata']
+            components = data['components']
+            # test for valid metadata
+            test = ValidateMetadata(uri, metadata)
+            rules_results.append(test)
 
         if test.passed:
             for component in components:
+                # validate the components
                 test = ValidateComponent(uri, component)
                 rules_results.append(test)
                 if not test.passed:
-                    # raise RuleInheritanceError(f'Failures: {len(test.fail_reasons)}. Fail reason: {test.fail_reasons}.')
                     break
-                
-                failed = False # if any of the rules in the component fail, then stop
+
+                # test each rule
+                failed = False # if any of the rules fail, then stop
                 for rule in component['rules']:
-                    if rule == 'redirect':
-                        test = ValidateRedirect(metadata, component)
+                    if rule == 'html':
+                        test = ValidateHTML(uri, metadata, component)
                         rules_results.append(test)
-                        # if not test.passed:
-                            # raise RuleInheritanceError(f'Failures: {len(test.fail_reasons)}. Fail reason: {test.fail_reasons}.')
-                            # rules_results.append(test)
-                    if rule == 'html_title':
-                        test = ValidateHTMLResponse(uri, metadata, component)
+
+                    if rule == 'rdf':
+                        test = ValidateRDF(uri, metadata, component)
                         rules_results.append(test)
-                    if not test.passed:
-                        failed = True
-                        break
+                #     if rule == 'html':
+                #         # test = ValidateRedirect(metadata, component)
+                #         test = ValidateHTML()
+                #         rules_results.append(test)
+
+                    # if rule == 'html_title':
+                    #     test = ValidateHTMLResponse(uri, metadata, component)
+                    #     rules_results.append(test)
+                    #
+                    # if rule == 'rdf':
+                    #     test = ValidateTurtleResponse('hello', metadata, component)
+                    #
+                    # # if any tests failed, stop
+                    # if not test.passed:
+                    #     failed = True
+                    #     break
 
 
-                if failed: # if a component fails, stop
-                    break
+                # if failed: # if a component fails, stop
+                #     break
 
         RuleSet.__init__(self, ruleset_name, rules_results)
 
 
-class ValidateTurtleResponse(Rule):
-    """Validate the RDF Content-type as text/turtle and that the title in the RDF document matches the expected title.
-    """
 
+class ValidateRDF(Rule):
     def __init__(self, uri:str, metadata:dict, component:list):
-        """Constructor
-        
-        Arguments:
-            uri {str} -- The URI.
-            metadata {dict} -- The metadata of the test.
-            component {list} -- The component containing the expected title.
-        """
-
         passed = True
         fail_reasons = []
+        total_components = 1 #TODO:
 
-         # get the RDF version of the resource, using Content Negotiation
-        r = requests.get(uri, headers={'Accept': 'text/turtle'})
+        ### turtle file-extension-like
+        _from = component['from'] + '.ttl'
+        _to = component['to'] + '.ttl'
+        _headers = {}  # {"Accept": "text/turtle"}
 
-        # check Content-Type
-        # assert 'text/turtle' in r.headers['Content-Type'], \
-        #     'Response for RDF by Content Negotiation does not have Content-Type set to text/turtle.'
-        if r.headers['Content-Type'] == 'text/turtle':
-            pass
+        # check the redirect for turtle
+        r = requests.get(_from, _headers, allow_redirects=False, timeout=2)
+        if (r.headers.get('Location') == _to):
+            # check that the content type is set correctly
+            if r.headers['Content-Type'] is not 'text/turtle':
+                passed = False
+                fail_reasons.append(f'{_from} Response for turtle by file extension does not have Content-Type set to text/turtle')
+            else:
+                # check that the RDF is parsable and that it contains the expected title
+                result = self.is_rdf_parsable(self, r, uri, component['title'], 'text/turtle')
+                if not result['titleFound']:
+                    passed = False
+                    theFoundTitle = result['title']
+                    fail_reasons.append(f'{_from} RDF title found as \'{theFoundTitle}\'' + result['message'])
         else:
             passed = False
-            fail_reasons.append('Response for RDF by Content Negotiation does not have Content-Type set to text/turtle.')
+            fail_reasons.append(
+                'For test \'{}\', URI {} did not redirect to {} as expected, instead {}.'.format(component['label'],
+                                                                                                 _from,
+                                                                                                 _to, str(
+                        r.headers.get('Location'))))
 
-        # get the RDF version of the resource, using Query String Arguments
-        r = requests.get(uri + '?_format=text/turtle')
+        ### END of turtle file-extension-like
 
-        # check Content-Type
-        # assert 'text/turtle' in r.headers['Content-Type'], \
-        #     'Response for RDF by _format Query String Argument does not have Content-Type set to text/turtle.'
-        if r.headers['Content-Type'] == 'text/turtle':
-            pass
+        ### turtle QSA
+        _from = component['from'] + '?_format=text/turtle'
+
+        # check the redirect for turtle
+        r = requests.get(_from, _headers, allow_redirects=False, timeout=2)
+        if (r.headers.get('Location') == _to):
+            # check that the content type is set correctly
+            if r.headers['Content-Type'] is not 'text/turtle':
+                passed = False
+                fail_reasons.append(f'{_from} Response for turtle by QSA does not have Content-Type set to text/turtle')
+            else:
+                # check that the RDF is parsable and that it contains the expected title
+                result = self.is_rdf_parsable(self, r, uri, component['title'], 'text/turtle')
+                if not result['titleFound']:
+                    passed = False
+                    theFoundTitle = result['title']
+                    fail_reasons.append(f'{_from} RDF title found as \'{theFoundTitle}\'' + result['message'])
         else:
             passed = False
-            fail_reasons.append('Response for RDF by _format Query String Argument does not have Content-Type set to text/turtle.')
+            fail_reasons.append(
+                'For test \'{}\', URI {} did not redirect to {} as expected, instead {}.'.format(component['label'],
+                                                                                                 _from,
+                                                                                                 _to, str(
+                        r.headers.get('Location'))))
+        ### END of turtle QSA
 
-        # get the RDF version of the resource, using file extension-like syntax
-        r = requests.get(uri  + '.ttl')
+        ### turtle conneg
+        _headers = {"Accept": "text/turtle"}
+        _from = component['from']
+        _to = component['to'] + '.ttl'
 
-        # check Content-Type
-        # assert 'text/turtle' in r.headers['Content-Type'], \
-            # 'Response for RDF by file extension does not have Content-Type set to text/turtle.'
-        if r.headers['Content-Type'] == 'text/turtle':
-            pass
+        r = requests.get(_from, headers=_headers, allow_redirects=False, timeout=2)
+        if r.headers.get('Location') == _to:
+            # check that the content type is set correctly
+            if r.headers['Content-Type'] is not 'text/turtle':
+                passed = False
+                fail_reasons.append(f'{_from} Response for turtle by content negotiation does not have Content-Type set to text/turtle')
+            else:
+                # check that the RDF is parsable and that it contains the expected title
+                result = self.is_rdf_parsable(self, r, uri, component['title'], 'text/turtle')
+                if not result['titleFound']:
+                    passed = False
+                    theFoundTitle = result['title']
+                    fail_reasons.append(f'{_from} RDF title found as \'{theFoundTitle}\'' + result['message'])
         else:
             passed = False
-            fail_reasons.append('Response for RDF by file extension does not have Content-Type set to text/turtle.')
+            fail_reasons.append(
+                'For test \'{}\', URI {} did not redirect to {} as expected, instead {}.'.format(component['label'],
+                                                                                                 _from,
+                                                                                                 _to, str(
+                        r.headers.get('Location'))))
+        ## END of turtle conneg
+
+        Rule.__init__(
+            self,
+            metadata['name'],
+            'Validate expected RDF',
+            metadata['authority'],
+            passed,
+            fail_reasons,
+            total_components,
+            len(fail_reasons)
+        )
+
+    def is_rdf_parsable(self, r, uri, expected_title, formatType):
+        # check the result is parseable RDF
+        g = None
+        try:
+            g = rdflib.Graph().parse(data=r.content.decode('utf-8'), format=formatType)
+        except Exception as e:
+            print(e)
+
+        if g is None:
+            raise AssertionError(f'RDF {formatType} result is not parsable')
+
+        # define namespaces
+        RDFS = rdflib.namespace.RDFS
+        XSD = rdflib.namespace.XSD
+        SKOS = rdflib.namespace.SKOS
+
+        # dict of result to return
+        result = {'titleFound': False}
+
+        # test to get title string
+        for s, p, o in g.triples((rdflib.URIRef(uri), RDFS.label, None)):
+            if o is not None:
+                if o.value == expected_title:
+                    result['titleFound'] = True
+                result['title'] = o
+
+        for s, p, o in g.triples((None, SKOS.prefLabel, None)):
+            if o is not None:
+                if o.value == expected_title:
+                    result['titleFound'] = True
+                result['title'] = o
+
+        for s, p, o in g.triples((None, SKOS.label, None)):
+            if o is not None:
+                if o.value == expected_title:
+                    result['titleFound'] = True
+                result['title'] = o
+
+        result['message'] = 'Parsable RDF response does not contain correct ontology title (?o  a owl:Ontology ; rdfs:label ?title .) or the expected title did not match.'
+
+        return result   # returns the title that was found and whether it succeeded or not
+                        # result['title'] = the title that was found
+                        # result['titleFound'] = the bool if title was found
 
 
-        result = is_rdf_parseable(r, uri, expected_title, 'text/turtle')
-        print(f'Title found: {result}')
+# class ValidateTurtleResponse(Rule):
+#     """Validate the RDF Content-type as text/turtle and that the title in the RDF document matches the expected title.
+#     """
+#
+#     def __init__(self, uri:str, metadata:dict, component:list):
+#         """Constructor
+#
+#         Arguments:
+#             uri {str} -- The URI.
+#             metadata {dict} -- The metadata of the test.
+#             component {list} -- The component containing the expected title.
+#         """
+#
+#         passed = True
+#         fail_reasons = []
+#
+#          # get the RDF version of the resource, using Content Negotiation
+#         r = requests.get(uri, headers={'Accept': 'text/turtle'})
+#
+#         # check Content-Type
+#         # assert 'text/turtle' in r.headers['Content-Type'], \
+#         #     'Response for RDF by Content Negotiation does not have Content-Type set to text/turtle.'
+#         if r.headers['Content-Type'] == 'text/turtle':
+#             pass
+#         else:
+#             passed = False
+#             fail_reasons.append('Response for RDF by Content Negotiation does not have Content-Type set to text/turtle.')
+#
+#         # get the RDF version of the resource, using Query String Arguments
+#         r = requests.get(uri + '?_format=text/turtle')
+#
+#         # check Content-Type
+#         # assert 'text/turtle' in r.headers['Content-Type'], \
+#         #     'Response for RDF by _format Query String Argument does not have Content-Type set to text/turtle.'
+#         if r.headers['Content-Type'] == 'text/turtle':
+#             pass
+#         else:
+#             passed = False
+#             fail_reasons.append('Response for RDF by _format Query String Argument does not have Content-Type set to text/turtle.')
+#
+#         # get the RDF version of the resource, using file extension-like syntax
+#         r = requests.get(uri  + '.ttl')
+#
+#         # check Content-Type
+#         # assert 'text/turtle' in r.headers['Content-Type'], \
+#             # 'Response for RDF by file extension does not have Content-Type set to text/turtle.'
+#         if r.headers['Content-Type'] == 'text/turtle':
+#             pass
+#         else:
+#             passed = False
+#             fail_reasons.append('Response for RDF by file extension does not have Content-Type set to text/turtle.')
+#
+#
+#         result = is_rdf_parseable(r, uri, expected_title, 'text/turtle')
+#         print(f'Title found: {result}')
 
 
-class ValidateHTMLResponse(Rule):
+class ValidateHTML(Rule):
     """Validate the HTML Content-type and title tag of the PID URI.
     """
 
@@ -255,6 +414,18 @@ class ValidateHTMLResponse(Rule):
 
         passed = True
         fail_reasons = []
+
+        # check the redirect
+        r = requests.get(component['from'], headers={}, allow_redirects=False, timeout=2)
+        if (r.headers.get('Location') == component['to']):
+            pass
+        else:
+            passed = False
+            fail_reasons.append(
+                'For test \'{}\', URI {} did not redirect to {} as expected, instead {}.'.format(component['label'],
+                                                                                                 component['from'],
+                                                                                                 component['to'], str(
+                        r.headers.get('Location'))))
 
         # get the HTML version of the resource
         r = requests.get(uri)
@@ -279,7 +450,7 @@ class ValidateHTMLResponse(Rule):
         Rule.__init__(
             self,
             metadata['name'],
-            'Validate expected HTML title tag',
+            'Validate expected HTML',
             metadata['authority'],
             passed,
             fail_reasons,
@@ -289,7 +460,7 @@ class ValidateHTMLResponse(Rule):
 
 
 class ValidateComponent(Rule):
-    """Validate that the PID URI test contains the correct component fields.
+    """Validate that the PID URI test contains the correct component keys.
     
     Arguments:
         Rule {[type]} -- [description]
@@ -299,14 +470,14 @@ class ValidateComponent(Rule):
         passed = True
         fail_reasons = []
 
-        # required component fields
+        # required component keys
         required = [
             'label',
             'from',
             'to',
-            'headers',
             'title',
-            'rules'
+            'rules',
+            'headers'
         ]
 
         for req in required:
@@ -329,6 +500,7 @@ class ValidateComponent(Rule):
 
 
 class ValidateRedirect(Rule):
+    #TODO: This is not currently used
     """Validate expected redirect route from a location to a destination. Redirects consider HTTP headers for content negotiation.
     """
 
@@ -361,7 +533,30 @@ class ValidateRedirect(Rule):
             1, # total components
             len(fail_reasons)
         )
-        
+
+
+class ValidateMetadataIsPresent(Rule):
+    def __init__(self, uri:str, data:dict):
+        passed = True
+        fail_reasons = []
+
+        if not "metadata" in data:
+            fail_reasons.append(f'The metadata field is missing for URI {uri}')
+
+        if fail_reasons:
+            passed = False
+
+        Rule.__init__(
+            self,
+            'Metadata field is present for AGLDWG PID URI',
+            'Validate that the AGLDWG URI contains a field named \'metadata\'',
+            'AGLDWG',
+            passed,
+            fail_reasons,
+            1,
+            len(fail_reasons)
+        )
+
 
 class ValidateMetadata(Rule):
     """Validate that the PID URI test contains the required metadata.
--- myrulesets/test_agldwg_ruleset.py
+++ myrulesets/test_agldwg_ruleset.py
@@ -4,36 +4,45 @@ import json
 from pprint import pprint
 from agldwg_ruleset import AGLDWG_ValidRuleSet
 
-# marshalling function
-# add in array of results
+
 class TestAGLDWG_Ruleset:
+    # marshalling function
+    # add in array of results
 
     def __init__(self):
-        self.result = None
+        self.result = []
 
-        filename = 'test.linked.data.gov.au.json'
+        filename = 'test2.linked.data.gov.au.json'
         file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)
 
+        # load JSON file
         uris_object = None
         with open(file_path, 'r') as f:
             uris_object = json.load(f)
 
+        # call the ruleset with the test info
         for key, val in uris_object.items():
-            # pprint(key)
-            # pprint(val)
-            self.result = AGLDWG_ValidRuleSet(key, val)
-            
+            self.result.append(AGLDWG_ValidRuleSet(key, val))
             # break
-        # pprint(uris_object)
+
 
 if __name__ == '__main__':
 
     # Test driver
     test = TestAGLDWG_Ruleset()
 
-    
-    if test.result.passed:
+    failed = False
+    for result in test.result:
+        if not result.passed:
+            failed = True
+
+    if not failed:
         print('All passed!')
     else:
         print('Test failed.')
-        print(test.result.fail_reasons)
+        for reason in test.result:
+            if len(reason.fail_reasons) is not 0:
+                for fail in reason.fail_reasons:
+                    pprint(fail)
+                    print()
+                # pprint(reason.fail_reasons)
