587773715f6669c6ad5b35493ac300b90c554302
Nicholas Car <nicholas.car@csiro.au>
2019-05-07 14:44:21 +1000

    catalogue tester

--- tests/linked.data.gov.au.py
+++ tests/linked.data.gov.au.py
@@ -1,7 +1,6 @@
-# this file will contain all the tests for linked.data.gov.au
+import tests.functions as functions
 
 if __name__ == "__main__":
-    import functions
 
     functions.validate_all_redirects("linked.data.gov.au.json")
     # functions.validate_turtle_response('http://linked.data.gov.au/def/placenames', 'Place Names Ontolodgy')
