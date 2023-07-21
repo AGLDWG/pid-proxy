5ced169521d654b16fccf14e20e6ed5f8890b7d0
AGLDWG Rewriter staging <Richard.Walker@ardc.edu.au>
2023-07-21 14:53:35 +1000

    Update Geofabric and G-NAF test data, support ignoring a test failure
    Update Geofabric and G-NAF test data to match the current reality.
    
    Support ignoring a test failure, using '"ignore_failure": true'.
    Add some comments to some tests.

--- test-suite/tests/functions_keep_going.py
+++ test-suite/tests/functions_keep_going.py
@@ -9,16 +9,23 @@ except ImportError:
 import json
 
 
-def validate_redirect(json_file, label, from_, to, headers=None):
+def validate_redirect(json_file, label, from_, to, headers, case):
     r = requests.get(from_, headers=headers, allow_redirects=False, timeout=2)
 
     if (r.headers.get("Location") == to):
         print("OK\n\n")
     else:
-        print("NOT OK\n\n")
-        print("For file '{}', test '{}', URI {} did not redirect to {} as expected, instead {}.".format(
-            json_file, label, from_, to, str(r.headers.get("Location"))), file = sys.stderr)
+        if (case.get("ignore_failure") is True):
+            print("NOT OK, but ignoring failure")
+            comment = case.get("comment")
+            if comment is not None:
+                print("comment: ", comment)
+            print("\n")
 
+        else:
+            print("NOT OK\n\n")
+            print("For file '{}', test '{}', URI {} did not redirect to {} as expected, instead {}.".format(
+                json_file, label, from_, to, str(r.headers.get("Location"))), file = sys.stderr)
 
 def validate_html_response(label, uri, expected_title):
     # get the HTML version of the resource
@@ -189,4 +196,4 @@ def validate_all_redirects(json_file):
             print("from: {}\nto: {}".format(case["from"], case["to"]))
             if len(case["headers"]) > 0:
                 print("headers: {}".format(case["headers"]))
-            validate_redirect(json_file, case["label"], case["from"], case["to"], case["headers"])
+            validate_redirect(json_file, case["label"], case["from"], case["to"], case["headers"], case)
