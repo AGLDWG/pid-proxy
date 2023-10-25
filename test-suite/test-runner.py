4c775f32eb532c63cb490f791f5c66071667566f
Nicholas Car <nick@kurrawong.net>
2023-10-25 17:13:24 +1000

    add a few TERN tests

--- test-suite/test-runner.py
+++ test-suite/test-runner.py
@@ -5,7 +5,5 @@ if __name__ == "__main__":
     functions.validate_all_redirects("linked.data.gov.au/org/abs.json")
     functions.validate_all_redirects("linked.data.gov.au/org/gsq.json")
     functions.validate_all_redirects("linked.data.gov.au/org/gswa.json")
-    # functions.validate_turtle_response('http://linked.data.gov.au/def/placenames', 'Place Names Ontolodgy')
-    # functions.validate_turtle_response('http://reference.data.gov.au/def/ont/agrif', 'The Australian Government Records Interoperability Framework (AGRIF) ontology')
-    # functions.validate_turtle_response('http://gnafld.net/def/gnaf', 'GNAF ontology')
-    # functions.validate_turtle_response('https://gist.githubusercontent.com/edmondchuc/ea2cc0bb2ce8f0f2adfa583a34540153/raw/a53f3a550c96cba9098ef033aec001ac6511ee1b', 'pizza')
+    functions.validate_all_redirects("linked.data.gov.au/org/tern.json")
+
